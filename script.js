8b477c4b6c53906203284abd6fd6212a40cd900d
szopen1228 <67273348+szopen1228@users.noreply.github.com>
2023-01-31 11:58:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let logo = document.getElementById('logo');
 let text = document.getElementById('text');
-let bg = document.getElementById('Portfolio');
+let bg = document.getElementById('background');
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
     logo.style.marginTop = value * 1 + 'px';
-    bg.style.backgroundPositionY = value * 0.5 + 'px';
+    bg.style.backgroundPositionY = value * 1 + 'px';
 });
 
 var slider = document.getElementById("myRange");
